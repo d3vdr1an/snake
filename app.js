f4d0223a8fde2c0226a76af7e2ed942bab192ab5
d3vdr1an <d3vdr1an@gmail.com>
2021-03-06 20:10:22 +1300

    Increase initial speed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentSnake = [2,1,0] 
     let direction = 1
     let score = 0
-    let speed = 1.15
+    let speed = 0.9
     let intervalTime = 0
     let interval = 0
   
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
       randomApple()
       direction = 1
       scoreDisplay.innerText = score
-      intervalTime = 1000
+      intervalTime = 500
       currentSnake = [2,1,0]
       currentIndex = 0
       currentSnake.forEach(index => squares[index].classList.add('snake'))
